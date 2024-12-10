1b15a9f6e95e03bead49b801c0b0126e8d2d60e9
h1ne <henryharlay@gmail.com>
2024-12-10 23:38:16 +0900

    Convert to module

--- module/getData.py
+++ module/getData.py
@@ -1,27 +1,41 @@
 import sqlite3
+from dotenv import load_dotenv
 
-# データベースに接続
-conn = sqlite3.connect('p1_words.db')
+load_dotenv()
+
+conn = sqlite3.connect(os.getenv(DB_FILE))
 c = conn.cursor()
 
-# word_id に対応する単語と意味を取得
-i = 35
+def GetWord(i):
+    c.execute("SELECT word FROM words WHERE word_id=:word_id", {'word_id': num})
+    word = c.fetchone()
+    if word:  # 結果がある場合
+        # print(word[0])  # タプルの最初の要素を取得
+        return word[0]
+    else:
+        return "ERROR: Word not found."
 
-# 単語を取得
-c.execute("SELECT word FROM words WHERE word_id=:word_id", {'word_id': i})
-word = c.fetchone()
-if word:  # 結果がある場合
-    print(word[0])  # タプルの最初の要素を取得
-else:
-    print("Word not found.")
 
-# 意味を取得
-c.execute("SELECT meaning FROM words WHERE word_id=:word_id", {'word_id': i})
-meaning = c.fetchone()
-if meaning:  # 結果がある場合
-    print(meaning[0])  # タプルの最初の要素を取得
-else:
-    print("Meaning not found.")
+def GetMeaning(i):
+    # 意味を取得
+    c.execute("SELECT meaning FROM words WHERE word_id=:word_id", {'word_id': num})
+    meaning = c.fetchone()
+    if meaning:  # 結果がある場合
+        # print(meaning[0])  # タプルの最初の要素を取得
+        return meaning[0]
+    else:
+        return "ERROR: Meaning not found."
 
-# 接続を閉じる
-conn.close()
+
+def GetData(mode, num): # Mode 0: val1<-Word, val2<-Meaning Mode 1: Reverse
+    val1 = GetWord(num)
+    val2 = GetMeaning(num)
+    if mode == 0:
+        return val1, val2 # Word, Meaning
+    elif mode == 1:
+        return val2, val1 # Meaning, Word
+
+if __name__ == '__main__':
+    print("hello")
+
+conn.close() # 接続を閉じる
