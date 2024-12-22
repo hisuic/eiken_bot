9f51ddd14e8f688bb4d52da337283968bcb98aee
h1ne <henryharlay@gmail.com>
2024-12-22 21:24:08 +0900

    make word test submodule

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import os
 
 from dotenv import load_dotenv
+from module.word_test import WordTest
 from module.button import Button2, Button4
 
 load_dotenv()
@@ -22,15 +23,7 @@ async def hello(ctx):
 
 @client.command()
 async def test(ctx):
-    # Viewのインスタンス生成時に引数を渡す (例: question="appleの意味は？")
-    view = Button2(value1="value1", value2="value2")
-    # メッセージと一緒にViewを送信
-    await ctx.send("次の問題に回答してください！", view=view)
-
-    # Viewのボタンが押される (またはタイムアウト) まで待機
-    await view.wait()
-
-    print(view.selected)
+    await WordTest(ctx)
 
 TOKEN = os.getenv('TOKEN')
 client.run(str(TOKEN))
--- module/word_test.py
+++ module/word_test.py
@@ -1,3 +1,12 @@
 import discord
 
 from module.button import Button2, Button4
+
+async def WordTest(ctx):
+    # Viewのインスタンス生成時に引数を渡す (例: question="appleの意味は？")
+    view = Button2(value1="value1", value2="value2")
+    # メッセージと一緒にViewを送信
+    await ctx.send("次の問題に回答してください！", view=view)
+    # Viewのボタンが押される (またはタイムアウト) まで待機
+    await view.wait()
+    print(view.selected)
