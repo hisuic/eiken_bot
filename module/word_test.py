bfcedc72f83c741e9082c22210a37ae40073df10
h1ne <henryharlay@gmail.com>
2024-12-22 22:18:50 +0900

    add button3

--- module/word_test.py
+++ module/word_test.py
@@ -1,12 +1,19 @@
 import discord
 
-from module.button import Button2, Button4
+from module.button import Button2, Button3, Button4
+
+# =========================================================================
+# # Viewのインスタンス生成時に引数を渡す (例: question="appleの意味は？")
+# view = Button2(value1="value1", value2="value2")
+# # メッセージと一緒にViewを送信
+# await ctx.send("次の問題に回答してください！", view=view)
+# # Viewのボタンが押される (またはタイムアウト) まで待機
+# await view.wait()
+# print(view.selected)
+# =========================================================================
 
 async def WordTest(ctx):
-    # Viewのインスタンス生成時に引数を渡す (例: question="appleの意味は？")
     view = Button2(value1="value1", value2="value2")
-    # メッセージと一緒にViewを送信
     await ctx.send("次の問題に回答してください！", view=view)
-    # Viewのボタンが押される (またはタイムアウト) まで待機
     await view.wait()
     print(view.selected)
